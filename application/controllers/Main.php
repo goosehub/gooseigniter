22976b57d91f957a50faba6398f13925159755cf
Goose <goosepostbox@gmail.com>
2017-05-29 16:41:12 -0400

    Improved request middleware
    Implement user_flag, user_key, and api_flag as well as some rename and
    fix check route.

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -6,26 +6,24 @@ class Main extends CI_Controller {
     function __construct()
     {
         parent::__construct();
+
         // Uncomment models after database created
         // $this->load->model('main_model', '', TRUE);
         // $this->load->model('user_model', '', TRUE);
 
-        // $user_flag = 0;
-        // $user_key = 0;
-        // $api_flag = 0;
-        // $this->main_model->insert_request($user_flag, $user_key, $api_flag);
+        // $this->main_model->record_result();
     }
 
     public function index()
     {
         // Authentication
-        $log_check = $data['log_check'] = $data['user_id'] = false;
+        $log_check = $data['log_check'] = $data['user'] = false;
         if ($this->session->userdata('logged_in')) {
             $log_check = $data['log_check'] = true;
             $session_data = $this->session->userdata('logged_in');
-            $user_id = $data['user_id'] = $session_data['id'];
+            $user_id = $session_data['id'];
             $data['user'] = $this->user_model->get_user($user_id);
-            if (! isset($data['user']['username']) ) {
+            if (!isset($data['user']['username'])) {
                 redirect('user/logout', 'refresh');
                 return false;
             }

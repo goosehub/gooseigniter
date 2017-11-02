52264a0cf7d302912d319c0a8cecce070ada0fdb
Goose <goosepostbox@gmail.com>
2017-11-01 21:50:31 -0400

    Tweak reserved usernames
    Don't need the _, that's small crowd, big world specific.

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -122,7 +122,7 @@ class User extends CI_Controller {
         // Disallow usernames we reserve
         if (
             strtolower($username) === 'anonymous' ||
-            strpos(strtolower($username), strtolower('system_')) !== false ||
+            strpos(strtolower($username), strtolower('system')) !== false ||
             strpos(strtolower($username), strtolower('admin')) !== false) {
             $this->form_validation->set_message('register_validation', 'Username is reserved');
             return false;
